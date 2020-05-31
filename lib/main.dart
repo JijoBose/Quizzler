3087f5d86a681fdc2e5df69d95adf3c23a045d48
Jijo Bose <bosejijo@gmail.com>
2020-05-31 19:27:24 +0530

    Initial code

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900
+      )
+    );
+  }
+}
